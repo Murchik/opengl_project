33d2247fb685df8f4a0f2aadb2768a6f904cfa6f
Murchik <mur4ikoff@gmail.com>
2022-01-05 20:45:45 +0300

    fixed GLFW premake

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,15 @@
 workspace "opengl_project"
    architecture "x64"
    configurations { "Debug", "Release" }
+   startproject "Sandbox"
 
 outputdirname = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 group "Dependencies"
    include "opengl_project/lib/GLAD"
-	include "opengl_project/lib/GLFW"
+	include "opengl_project/lib" -- GLFW premake file
 group ""
 
 include "opengl_project"
 include "Sandbox"
 
-startproject "Sandbox"
